b8be18d2b5c3ad3ed646a6f65d50b8abb4750f90
nepthius <98651890+nepthius@users.noreply.github.com>
2022-06-28 19:22:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,68 @@
-/*function imageCycle(content, contentParent, arr){
-    let image = new Image();
-    image.src = arr[0]
-    contentParent.prepend($("<h1>BELIEVE IN YOURSELF</h1>.").addClass("beautText"), image);
-    let timer = 0;
-    for(let i = 1; i < arr.length; i++){
-        timer += 10000;
-        image.src = arr[i];
-        setTimeout(() => {
-            content.remove()
-            contentParent.prepend($("<h1>BELIEVE IN YOURSELF</h1>.").addClass("beautText"), image);
-        }, timer)
-    }
-    return 
+/* Additional Functionality Notes
+You can track what a user deletes and save it to a database for user reference and also include a button
+that resets user deletions */
+function randomNum(min, max){
+    return Math.random()*(max-min) + min;
 }
-*/
 
 if(document.URL.includes("youtube.com")){
     //const ycontents = document.getElementById('contents');
     //ycontents.parentNode.removeChild(ycontents);
     //For loop that uses set timer and traverses an array of images to fade in and fade out of the site
+    
+    //grabs contents
     let ycontents = $('#contents');
+
+    //grabs parent of contents
     let ycontentsParent = ycontents.parent();
+
+    //removes contents
     ycontents.remove();
-    let image = new Image();
-    let url = ("https://images.freeimages.com/images/large-previews/e33/tate-weather-project-8-1473995.jpg")
-    image.src = url
-    ycontentsParent.prepend($($("<h1>BELIEVE IN YOURSELF</h1>.").addClass("beautText")).animate({
-        left:'500px'
-    }), image)
+
+    //grabs an image from the unsplash API and displays it
+    let url = ""
+    
+    $.ajax({
+        method: 'GET',
+        url: 'https://picsum.photos/list',
+        success: function(result){
+            const image = result[randomNum(0, result.length)];
+            let dis = new Image()
+            url = 'https://unsplash.it/1200/800?image=' + image.id
+            dis.url = url;
+            //$('<img>').attr('src','https://unsplash.it/1200/800?image=' + image.id )
+            ycontentsParent.prepend(dis)
+        },
+        error: function(err){
+            console.log(err)
+        }
+    })
+    /*$($("<h1>BELIEVE IN YOURSELF</h1>.").addClass("beautText")).animate({
+                left:'500px'
+            }), 
+    */
+
+    
+    
+    
+    /* attempted to implement disappearing click, research more on this
+    let yid = "none";
+    $("body").click(function() {
+        alert(yid = $(this).attr('id'))
+        yid.remove()
+      });
+    */
+
+    //setTimeout(function() { alert('I am delayed') }, 3000);  
     //var url = []
     //url.push("https://images.freeimages.com/images/large-previews/e33/tate-weather-project-8-1473995.jpg");
     //imageCycle(ycontents, ycontentsParent, url)
     //let image = new Image();
-    //image.src = url;
-    
-    
+    //image.src = url;  
 }
-
 
 else if(document.URL.includes("reddit.com")){
     const rcontents = document.getElementById("AppRouter-main-content");
     rcontents.parentNode.removeChild(rcontents);
 }
-
-
 
