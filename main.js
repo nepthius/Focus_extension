04808719944f5018768aae9d1c74febdd1a46027
nepthius <98651890+nepthius@users.noreply.github.com>
2022-06-29 11:36:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,62 @@
-/*function imageCycle(content, contentParent, arr){
-    let image = new Image();
-    image.src = arr[0]
-    contentParent.prepend($("<h1>BELIEVE IN YOURSELF</h1>.").addClass("beautText"), image);
-    let timer = 0;
-    for(let i = 1; i < arr.length; i++){
-        timer += 10000;
-        image.src = arr[i];
-        setTimeout(() => {
-            content.remove()
-            contentParent.prepend($("<h1>BELIEVE IN YOURSELF</h1>.").addClass("beautText"), image);
-        }, timer)
-    }
-    return 
+function randomNum(min, max){
+    return Math.random()*(max-min) + min;
 }
-*/
 
 if(document.URL.includes("youtube.com")){
-    //const ycontents = document.getElementById('contents');
-    //ycontents.parentNode.removeChild(ycontents);
-    //For loop that uses set timer and traverses an array of images to fade in and fade out of the site
+    
+    //grabs contents
     let ycontents = $('#contents');
-    let ycontentsParent = ycontents.parent();
-    ycontents.remove();
-    let image = new Image();
-    let url = ("https://images.freeimages.com/images/large-previews/e33/tate-weather-project-8-1473995.jpg")
-    image.src = url
-    ycontentsParent.prepend($($("<h1>BELIEVE IN YOURSELF</h1>.").addClass("beautText")).animate({
-        left:'500px'
-    }), image)
-    //var url = []
-    //url.push("https://images.freeimages.com/images/large-previews/e33/tate-weather-project-8-1473995.jpg");
-    //imageCycle(ycontents, ycontentsParent, url)
-    //let image = new Image();
-    //image.src = url;
-    
-    
-}
+    let ybar = $('#chips')
+    let yheader = $('#header')
+    let ybbar = $('#chips-wrapper')
 
+    //grabs parent of contents
+    let ycontentsParent = ycontents.parent();
+
+    //removes contents
+    ycontents.remove();
+    ybar.remove();
+    yheader.remove();
+    ybbar.remove();
+
+    //grabs an image from the unsplash API and displays it
+    let url = ""
+    $.ajax({
+        method: 'GET',
+        url: 'https://picsum.photos/list',
+        success: function(result){
+            let image = result[Math.floor(randomNum(0, result.length- 1))];
+            let dis = new Image()
+            url = 'https://unsplash.it/1200/800?image=' + image.id
+            dis.src = url;
+            ycontentsParent.prepend($($("<h1>BELIEVE IN YOURSELF</h1>.").addClass("beautText")).animate({
+                left:'500px'
+            }), dis)
+            //If the user clicks on the image it will display a new image
+            $("body").click(function(){
+                let cont = $('#body');
+                let pcont = cont.parent();
+                $(cont).fadeOut("slow")
+                image = result[Math.floor(randomNum(0, result.length- 1))];
+                url = 'https://unsplash.it/1200/800?image=' + image.id;
+                console.log("This is the image id: " + String(image.id))
+                dis.src = url;
+                console.log("This is the url: " + String(dis.src))
+                pcont.prepend($(dis).fadeIn("slow"))
+
+            })
+        },
+        error: function(err){
+            console.log(err)
+        }
+    })
+}
 
 else if(document.URL.includes("reddit.com")){
     const rcontents = document.getElementById("AppRouter-main-content");
     rcontents.parentNode.removeChild(rcontents);
 }
+
 
 
 
